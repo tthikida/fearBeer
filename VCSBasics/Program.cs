569d99a5bd4fefa9dbf92843630ff1c3fc0c5689
tamioTech <62820237+tamioTech@users.noreply.github.com>
2023-05-19 23:14:00 -0700

    beeeer

--- VCSBasics/Program.cs
+++ VCSBasics/Program.cs
@@ -6,7 +6,7 @@ namespace VCSBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello wookie!!!!");
+            Console.WriteLine("beeer");
         }
     }
 }
